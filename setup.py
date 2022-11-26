9c0b5a6289ec5cfda322853e171e7b7e2a3c2ae9
franciscovillaescusa <villaescusa.francisco@gmail.com>
2022-11-26 17:25:36 -0500

    update

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="Pylians",
-    version="0.3",
+    version="0.4",
     author="Francisco Villaescusa-Navarro",
     author_email="villaescusa.francisco@gmail.com",
     description="Python libraries for the analysis of numerical simulations",
